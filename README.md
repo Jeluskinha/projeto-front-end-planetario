a55baf626cf78c3dc8b15833c91f2f8ca25ad5dd
Sarah Costa <sarahvenske@gmail.com>
2022-08-30 10:08:51 -0300

    model file

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # m3-final-project-planetary
-Kenzie Academy's m3 final project 
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Global from "./styles/global";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Global />
+    </div>
+  );
+}
+
+export default App;
