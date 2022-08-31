30a3a09d6798d66e16ae9b09d4b92c9ec53f22db
Bruna <bruna.dall9741@gmail.com>
2022-08-31 19:11:48 -0300

    feat: dashboard css

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,5 +1,34 @@
 import styled from 'styled-components';
 
 export const HeaderContainer = styled.header`
+    width: 100%;
+    height: 10%;
+    min-height: 45px;
 
+    padding: 0 5%;
+
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+    justify-content: space-between;
+
+    background-color: rgba(22, 22, 22, 0.95);
+
+    .header_div-logo {
+        width: 50%;
+        height: 100%;
+    };
+
+    .header_div-extra {
+        width: 50%;
+        height: 100%;
+
+        padding-left: 5%;
+
+        gap: 3%;
+        display: flex;
+        align-items: center;
+        flex-direction: row;
+        justify-content: flex-end;
+    };
 `
